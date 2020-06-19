8616ee4bc9d3746ad9b9f3d900d35f421bd4a257
Lord-bus <raulbus073@gmail.com>
2020-06-19 00:05:11 -0300

    modificación archivo readme

--- readme.md
+++ readme.md
@@ -1,2 +1,26 @@
 # HomeCinema
-DB_Peliculas_Genero_Actores
+
+En esta Aplicación se podran hacer las siguientes Interaciones:
+
+Ver un Listado de peliculas
+
+Ver Peliculas Recomendadas y Aleatorias
+
+Ver el Detalle de cada una
+
+Buscar una pelicula
+
+
+
+Solo el Administrador puede realizar las siguientes Acciones:
+
+Agregar una pelicula
+
+Editar una peliculas
+
+Borrar una pelicula
+
+Ademas de ver el Listado y Los detalles
+
+
+Autor: Raul Bustamante
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,7 +32,7 @@
         <div class="card">
           <img src="/storage/{{$pelicula->avatar}}" class="card-img-top" alt="Card image">
           <div class="card-body">
-            <p class="card-text">Premios: {{$pelicula->title}}</p>
+            <p class="card-text"><b>{{$pelicula->title}}</b></p>
             <div class="">
               <a href="/detallePelicula/{{$pelicula->id}}" class="btn btn-info btn-left btn-sm">Ver Detalles</a>
               {{-- <a href="/detallePeliculas/{{$pelicula->id}}" class="btn btn-success btn-right btn-sm">Trailer</a> --}}
